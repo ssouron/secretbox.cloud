498e578c73e848706090742ceeaaa83269d81a00
Stephane Souron <stephane.souron@gmail.com>
2017-03-20 16:49:35 +0100

    ajout utilisateur stef à setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -2,8 +2,12 @@
 
 # paramétrage de la connexion au vRack
 
+echo
+read -p "Adresse IP de la connexion vRack : " IP
+
 echo > 90-vrack.cfg
 for n in 4 5 6 7 8
+do
   echo "auto ens$n" >> 90-vrack.cfg
   echo "iface ens$n inet static" >> 90-vrack.cfg
   echo "    address $IP" >> 90-vrack.cfg
@@ -15,6 +19,12 @@ sudo chown root:root 90-vrack.cfg
 sudo mv 90-vrack.cfg /etc/network/interfaces.d/
 
 
+# création de l'utilisateur stef
+sudo useradd -m -U -s /bin/bash stef
+sudo adduser stef sudo
+sudo cp -r /home/ubuntu/secretbox.cloud /home/stef/secretbox.cloud
+sudo chown -R stef:stef /home/stef/secretbox.cloud
+echo "stef ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/99-secretbox-users
 
 
 # redémarrage
