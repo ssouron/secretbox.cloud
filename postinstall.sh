c015d049de974f030ab173f4a2a59bdac88442f3
Stephane Souron <stephane.souron@gmail.com>
2017-03-20 19:02:44 +0100

    retour au static sur VLAN10

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IP='192.168.0.1'
+
 # installation git et clonage des scripts
 apt -y install git
 cd /home/ubuntu
@@ -11,7 +13,9 @@ echo > /etc/network/interfaces.d/99-vRack.cfg
 for n in 4 5 6 7 8
 do
   echo "auto ens$n" >> /etc/network/interfaces.d/99-vRack.cfg
-  echo "iface ens$n inet dhcp" >> /etc/network/interfaces.d/99-vRack.cfg
+  echo "iface ens$n inet static" >> /etc/network/interfaces.d/99-vRack.cfg
+  echo "    address $IP" >> /etc/network/interfaces.d/99-vRack.cfg
+  echo "    netmask 255.255.255.0" >> /etc/network/interfaces.d/99-vRack.cfg
 done
 
 
