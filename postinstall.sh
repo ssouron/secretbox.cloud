a0a174fcd38fe746372853fb41dd3e3169647b76
Stephane Souron <stephane.souron@gmail.com>
2017-03-20 18:50:57 +0100

    config réseau au postinstall en dhcp

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# installation git et clonage des scripts
 apt -y install git
-
 cd /home/ubuntu
-
 git clone https://github.com/ssouron/secretbox.cloud.git
 chown -R ubuntu:ubuntu /home/ubuntu/secretbox.cloud
 
+# configuration interface vRack
+echo > /etc/network/interfaces.d/99-vRack.cfg
+for n in 4 5 6 7 8
+do
+  echo "auto ens$n" >> /etc/network/interfaces.d/99-vRack.cfg
+  echo "iface ens$n inet dhcp" >> /etc/network/interfaces.d/99-vRack.cfg
+done
+
+
+
+
+# redémarrage
+reboot now
 
