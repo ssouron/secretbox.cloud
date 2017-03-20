8013fb5d45661e6123a57ee8c2f70ff3467e7b0c
Stephane Souron <stephane.souron@gmail.com>
2017-03-20 16:29:06 +0100

    debut de script setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-sudo -i
-whoami
+# paramétrage de la connexion au vRack
+
+echo > 90-vrack.cfg
+for n in 4 5 6 7 8
+  echo "auto ens$n" >> 90-vrack.cfg
+  echo "iface ens$n inet static" >> 90-vrack.cfg
+  echo "    address $IP" >> 90-vrack.cfg
+  echo "    netmask 255.255.255.0" >> 90-vrack.cfg
+  echo >> 90-vrack.cfg
+done
+
+sudo chown root:root 90-vrack.cfg
+sudo mv 90-vrack.cfg /etc/network/interfaces.d/
+
+
+
+
+# redémarrage
+sudo reboot now
 
