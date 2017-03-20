dbe465e2114eb23a94d7bec04d3c560939294688
Stephane Souron <stephane.souron@gmail.com>
2017-03-20 15:24:49 +0100

    git clone à l'installation

---
+++ postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt -y install git
+
+cd /home/ubuntu
+
+git clone https://ssouron@gitlab.com/ssouron/secretbox.cloud.git
+
--- whoami.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-whoami > /root/who.txt
-
-echo "host = $host" >> /root/who.txt
-echo "IP = $IP" >> /root/who.txt
-
