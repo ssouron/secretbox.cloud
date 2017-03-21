92cd866d9fb4cec4c633d7397b678fde9c748911
Stephane Souron <stephane.souron@gmail.com>
2017-03-21 01:10:14 +0100

    Interfaces reseau OK après reboot

--- postinstall.sh
+++ postinstall.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-IP='192.168.0.1'
+IP='192.168.0.87'
 
-# installation git et clonage des scripts
-apt -y install git
-cd /home/ubuntu
-git clone https://github.com/ssouron/secretbox.cloud.git
-chown -R ubuntu:ubuntu /home/ubuntu/secretbox.cloud
-
-# configuration interface vRack
-echo > /etc/network/interfaces.d/99-vRack.cfg
+echo > /etc/network/interfaces.d/99-vrack.cfg
 for n in 4 5 6 7 8
 do
-  echo "auto ens$n" >> /etc/network/interfaces.d/99-vRack.cfg
-  echo "iface ens$n inet static" >> /etc/network/interfaces.d/99-vRack.cfg
-  echo "    address $IP" >> /etc/network/interfaces.d/99-vRack.cfg
-  echo "    netmask 255.255.255.0" >> /etc/network/interfaces.d/99-vRack.cfg
+  echo "auto ens$n" >> /etc/network/interfaces.d/99-vrack.cfg
+  echo "iface ens$n inet static" >> /etc/network/interfaces.d/99-vrack.cfg
+  echo "    address $IP" >> /etc/network/interfaces.d/99-vrack.cfg
+  echo "    netmask 255.255.255.0" >> /etc/network/interfaces.d/99-vrack.cfg
+  echo >> /etc/network/interfaces.d/99-vrack.cfg
 done
 
-
-
-
-# redémarrage
-reboot now
 
